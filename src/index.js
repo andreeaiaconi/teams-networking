da168e1e7e185375aa9ef62f39e608210f8f2e03
Andreea Iaconi <iaconiandreea@gmail.com>
2025-01-21 17:43:50 +0000

    feat: add update team request functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import "./style.css";
 
+let editId;
+//  this value will be updated to another value every time
+//  we do a laod
+let allTeams = [];
+
 function $(selector) {
   return document.querySelector(selector);
 }
@@ -25,6 +30,18 @@ function deleteTeamRequest(id) {
     //  the next one"
   }).then(r => r.json());
 }
+
+function updateTeamRequest(team) {
+  // PUT teams-json/update
+  return fetch("http://localhost:3000/teams-json/update", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(team)
+  }).then(r => r.json());
+}
+
 //  gets team as a json object and returns it as a string
 function getTeamAsHTML(team) {
   return `<tr>
@@ -33,6 +50,7 @@ function getTeamAsHTML(team) {
             <td>${team.name}</td>
             <td>${team.url}</td>
             <td>
+              <button type="button" data-id="${team.id}" class="action-btn edit-btn">&#9998;</button>
               <button type="button" data-id="${team.id}" class="action-btn delete-btn">♻️</button>
             </td>
           </tr>`;
@@ -56,6 +74,7 @@ function loadTeams() {
     // then wait for the json response
     .then(r => r.json())
     .then(teams => {
+      allTeams = teams;
       renderTeams(teams);
       //  then print value in console to see if the fetch was successful
       // console.warn("teams?", teams);
@@ -67,23 +86,56 @@ function onSubmit(e) {
   //   Prevents the default form submission behavior (i.e., stops the page
   // from reloading when the form is submitted)
   e.preventDefault();
+
+  const team = getTeamValues();
+
+  if (editId) {
+    team.id = editId;
+    console.warn("should we edit?", editId, team);
+    updateTeamRequest(team).then(status => {
+      console.warn("status", status);
+      if (status.success) {
+        window.location.reload();
+      }
+    });
+  } else {
+    // chaining
+    createTeamRequest(team).then(status => {
+      //   console.warn("status", status);
+      if (status.success) {
+        window.location.reload();
+      }
+    });
+  }
+}
+
+function startEdit(id) {
+  editId = id;
+  const team = allTeams.find(team => team.id === id);
+  console.warn("edit", id, team);
+  setTeamValues(team);
+}
+
+function setTeamValues(team) {
+  $("input[name=promotion]").value = team.promotion;
+  $("input[name=members]").value = team.members;
+  $("input[name=name]").value = team.name;
+  $("input[name=url]").value = team.url;
+}
+
+function getTeamValues() {
+  const promotion = $("input[name=promotion]").value;
   const members = $("input[name=members]").value;
-  const name = $("#name").value;
-  const url = $("#url").value;
-  const team = {
-    promotion: $("input[name=promotion]").value,
+  const name = $("input[name=name]").value;
+  const url = $("input[name=url]").value;  
+  return {
+    promotion: promotion,
     members: members,
     name: name,
     url: url
   };
-  // chaining
-  createTeamRequest(team).then(status => {
-    //   console.warn("status", status);
-    if (status.success) {
-      window.location.reload();
-    }
-  });
 }
+
 
 function initEvents() {
   // select the element's id and add an event to it
@@ -96,7 +148,10 @@ function initEvents() {
           window.location.reload();
         }
       });
-    }
+    } else if (e.target.matches("button.edit-btn")) {
+      const id = e.target.dataset.id;
+      startEdit(id);
+    } 
   });
 }
 // calling the functions
